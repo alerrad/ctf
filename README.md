de5ca16c98659dbf84e1af961e5fa6c70bd4894b
Zhansen Segizbay <supersegizbay@gmail.com>
2023-06-12 20:50:36 +0600

    Refactored the code, Added a CLI, readme docs & requirements

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# CTF-gif-
+# __Reframer__
+
+Simple reframer srcipt with a CLI that let's you extract all the frames from a given GIF file.
+
+## Backstory
+This was basically the ctf home task. Purpose was to find out what was the given gif image meant by itself. Answer was that we had to extract all the frames and then translate each frame to a binary code.
+
+## Usage
+1. Clone this repo ```git clone https://github.com/alerrad/ctf```
+2. Go to the directory ```cd ctf```
+3. Set the python virtual env ```python -m venv .```
+4. Install dependencies ```pip install -r requirements.txt```
+5. Run the script ```python3 main.py```
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import tensorflow as tf
 import tensorflow_io as tfio
-from PIL import Image
-from PIL import GifImagePlugin
-
-# Load the GIF file
-FILENAME = "gman_1.gif"
-gif_data = tf.io.read_file(FILENAME)
-
-# Decode the GIF file using TensorFlow IO
-gif = tf.io.decode_gif(gif_data)
-
-flags = []
+from tqdm import tqdm
 
 
-for n, frame in enumerate(gif, start=1):
-    png_frame = tf.io.encode_png(frame)
-    frame_path = f"frames/frame_{n}.png"
-    tf.io.write_file(frame_path, png_frame)
+def extract_frames(gif_path: str, extracting_path: str) -> None:
+    frames = []
+
+    try:
+        gif_data = tf.io.read_file(gif_path)
+        frames = tf.io.decode_gif(gif_data)
+    except Exception as err:
+        print(err.with_traceback())
+        return
+
+    for n, frame in enumerate(tqdm(frames), start=1):
+        png_frame = tf.io.encode_png(frame)
+        frame_path = f"{extracting_path}/frame_{n}.png"
+        tf.io.write_file(frame_path, png_frame)
+
+
+if __name__ == "__main__":
+    gif_path = input("Path to gif file: ")
+    destination = input("Path for extracted frames: ")
+
+    extract_frames(gif_path, destination)
+    print("Extraction success!")
