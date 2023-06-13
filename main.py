7e547ddd00a98ffc6cb27272e6011bb64c93d79d
Zhansen Segizbay <supersegizbay@gmail.com>
2023-06-13 11:44:27 +0600

    threaded extractions

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from concurrent import futures
+
 import tensorflow as tf
 import tensorflow_io as tfio
 from tqdm import tqdm
 
 
-def extract_frames(gif_path: str, extracting_path: str) -> None:
+def extract_frame(frame: tf.Tensor, n: int, extracting_path: str) -> None:
+    png_frame = tf.io.encode_png(frame)
+    frame_path = f"{extracting_path}/frame_{n}.png"
+    tf.io.write_file(frame_path, png_frame)
+
+def extract_frames(gif_path: str, extracting_path: str = ".") -> None:
     frames = []
 
     try:
@@ -13,10 +20,12 @@ def extract_frames(gif_path: str, extracting_path: str) -> None:
         print(err.with_traceback())
         return
 
-    for n, frame in enumerate(tqdm(frames), start=1):
-        png_frame = tf.io.encode_png(frame)
-        frame_path = f"{extracting_path}/frame_{n}.png"
-        tf.io.write_file(frame_path, png_frame)
+    # Using a thread for each frame
+    with futures.ThreadPoolExecutor() as executor:
+        for n, frame in enumerate(tqdm(frames), start=1):
+            executor.submit(extract_frame, frame, n, extracting_path)
+
+    print("Extraction success!")
 
 
 if __name__ == "__main__":
@@ -24,4 +33,3 @@ if __name__ == "__main__":
     destination = input("Path for extracted frames: ")
 
     extract_frames(gif_path, destination)
-    print("Extraction success!")
